4c5e8b75bb009232d56f4ae373652729f586b948
Fvdliii <fgunawan528@gmail.com>
2026-04-03 13:33:27 +0800

    Pertemuan 2:blade template

---
+++ resources/views/student/create.blade.php
@@ -0,0 +1,5 @@
+<x-app>
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <div class="h1 fw-bold">Form Create Student</div>
+</x-app>
---
+++ resources/views/student/index.blade.php
@@ -0,0 +1,5 @@
+<x-app>
+    <x-slot:title>{{ $title }}</x-slot>
+
+    <div class="h1 fw-bold">Data Student</div>
+</x-app>
